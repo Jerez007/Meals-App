3f72e9a68fa3de1d1d8992bda58cdde055cb8e11
jerez007 <jerez.solis@gmail.com>
2024-01-30 21:46:10 -0800

    added grid styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,17 +1,53 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View, Platform } from "react-native";
 
-const CategoryGridTile = ({title, color}) => {
+const CategoryGridTile = ({ title, color }) => {
   return (
-    <View>
-      <Pressable>
-        <View>
-            <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color}]}>
+          <Text styl={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default CategoryGridTile
+export default CategoryGridTile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: "white", // needed to show shadow on iOS
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible", // makes sure ripple effect doesn't go beyond rounder corners
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5, //for iOS ripple effect
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-import CategoryGridTile from '../components/CategoryGridTile';
-import { CATEGORIES } from '../data/dummy-data.js';
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data.js";
 
 // defined outside since it doesn't have to be recreated
 function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 }
 
 export default function CategoriesScreen() {
-  return <FlatList data={CATEGORIES}  keyExtractor={(item) => item.id} renderItem={renderCategoryItem} />
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
-const styles = StyleSheet.create({})
-
+const styles = StyleSheet.create({});
