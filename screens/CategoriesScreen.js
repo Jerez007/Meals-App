3f11c7ad21db71c9652fdad6d1bc48b258c4c20d
jerez007 <jerez.solis@gmail.com>
2024-02-11 11:53:32 -0800

    added meals over screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,14 +2,21 @@ import { StyleSheet, Text, View, FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data.js";
 
-// defined outside since it doesn't have to be recreated
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+export default function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+        navigation.navigate('MealsOverview');
+    }
 
-export default function CategoriesScreen() {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
   return (
     <FlatList
       data={CATEGORIES}
