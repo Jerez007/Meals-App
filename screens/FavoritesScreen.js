6af353bebf744d53b9d838bb267d0afcf7dcfd42
jerez007 <jerez.solis@gmail.com>
2024-02-19 21:14:50 -0800

    finished app

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,23 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
-import { useContext } from "react";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { useContext } from "react";
+// import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 
 const FavoritesScreen = () => {
-	const favoriteMealsCtx = useContext(FavoritesContext);
+	// const favoriteMealsCtx = useContext(FavoritesContext);
+	const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
 	const favoriteMeals = MEALS.filter((meal) =>
-		favoriteMealsCtx.ids.includes(meal.id)
+		favoriteMealIds.includes(meal.id)
 	);
 
-  if (favoriteMeals.length === 0) {
-    return(
-      <View style={styles.rootContainer}>
-        <Text style={styles.text}>You have no favorite meals yet. </Text>
-      </View>
-    )
-  }
+	if (favoriteMeals.length === 0) {
+		return (
+			<View style={styles.rootContainer}>
+				<Text style={styles.text}>You have no favorite meals yet. </Text>
+			</View>
+		);
+	}
 
 	return <MealsList items={favoriteMeals} />;
 };
@@ -25,14 +27,14 @@ const FavoritesScreen = () => {
 export default FavoritesScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: 'white'
-  }
+	rootContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 18,
+		fontWeight: "bold",
+		color: "white",
+	},
 });
